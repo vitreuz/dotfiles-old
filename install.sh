c0a84c2597133ac317c82d97772a6611d3725435
vitreuz <svidrio@gmail.com>
2018-08-27 00:18:35 -0600

    Switch to neovim

--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
 #!/bin/bash -eu
 
-setup_directory() {
+setup_homedir() {
+  local config
+  config=${HOME}/.config
+
+  mkdir -p ${config}
+}
+
+setup_workspace() {
   local workspace
-	workspace=${1}/workspace
+  workspace=${1}/workspace
 
   echo "Creating workspace at '${workspace}'..."
-	mkdir -p $workspace
+  mkdir -p $workspace
+}
+
+setup_nvim() {
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
 
 link_files() {
@@ -16,20 +28,24 @@ link_files() {
   ln -sf $(pwd)/brew/Brewfile $home/.Brewfile
 
   echo "Linking '.gitconfig'..."
-  ln -sf $(pwd)/git/gitconfig $home/gitconfig
+  ln -sf $(pwd)/git/gitconfig $home/.gitconfig
 
-  echo "Linking 'vimrc'..."
-  ln -sf $(pwd)/vim/vimrc $home/.vimrc
+  echo "Linking 'nvim'..."
+  ln -sf $(pwd)/nvim/ $home/.config
 }
+
 
 main() {
   local home
   home=${1:-$HOME}
 
+  setup_homedir
   pushd ${HOME}
-    setup_directory ${home}
-    link_files
+    setup_workspace ${home}
   popd
+
+  setup_nvim
+  link_files
 }
 
 main
---
+++ nvim/init.vim
@@ -0,0 +1,12 @@
+call plug#begin()
+
+set number
+set relativenumber
+set showcmd
+
+set tabstop=2
+set shiftwidth=2
+set shiftround
+set expandtab
+
+call plug#end()
